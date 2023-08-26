55fc9c8d49901c7871c9f6f32f8d2c3f14230bec
Rohit-MK2K <rohitmalakar007@gmail.com>
2023-08-27 00:29:45 +0530

    defined routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import {BrowserRouter as Router,Route,Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Teams from './components/Teams'
+import Teams from './Pages/Teams'
 
 
 function App() {
@@ -11,6 +11,12 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+          <Route path='/' element={<h1 className="flex items-center h-full">This is Home Page go to Team page</h1>}/>
+          <Route path='/course' element={<h1>This is Course Page go to Team page</h1>}/>
+          <Route path='/selection' element={<h1>This is Selection Page go to Team page</h1>}/>
+          <Route path='/blog' element={<h1>This is blog Page go to Team page</h1>}/>
+          <Route path='/material' element={<h1>This is material Page go to Team page</h1>}/>
+          <Route path='/contact' element={<h1>This is contact Page go to Team page</h1>}/>
           <Route path='/teams' element={<Teams/> } />
         </Routes>
       </Router>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,27 +3,64 @@ import logo from '../assets/New-Sciastra-Logo.svg'
 import {NavLink} from "react-router-dom"
 
 function Navbar() {
-  return (
-    <>
-        <nav className='nav-class flex flex-row items-center h-20 justify-between px-10'>
-            <div className="name-container flex flex-row items-center justify-center text-2xl font-extrabold">
-               SciAstra   
-            </div>
-            <div className="logo-container w-12 h-12 flex flex-row items-center justify-center">
-               <img src={logo} alt="logo" /> 
-            </div>
-            <div className="nav-link-container flex flex-row items-center gap-4 ">
-                <NavLink>HOME</NavLink>
-                <NavLink>COURSES</NavLink>
-                <NavLink>SELECTION</NavLink>
-                <NavLink>BLOGS</NavLink>
-                <NavLink>MATERIALS</NavLink>
-                <NavLink>TEAM</NavLink>
-                <NavLink>CONTACT US</NavLink>
-            </div>
-        </nav>
-    </>
-  )
+    const activeNav = ''
+    const pendingNav = ''
+    return (
+        <>
+            <nav className='nav-class flex flex-row items-center h-20 justify-between px-10'>
+                <div className="name-container flex flex-row items-center justify-center text-2xl font-extrabold">
+                SciAstra   
+                </div>
+                <div className="logo-container w-12 h-12 flex flex-row items-center justify-center ">
+                <img src={logo} alt="logo" /> 
+                </div>
+                <div className="nav-link-container flex flex-row items-center gap-5 font-medium">
+                    <NavLink
+                        to = "/"
+                        className={({ isActive, isPending }) => isPending ? `pending` : isActive ? "active" : "nav-link"}
+                    >
+                        HOME
+                    </NavLink>
+                    <NavLink
+                        to = "/course"
+                        className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "nav-link"  }
+                    >
+                        COURSES
+                    </NavLink>
+                    <NavLink
+                        to = "/selection"
+                        className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "nav-link"}
+                    >
+                        SELECTION
+                    </NavLink>
+                    <NavLink
+                        to = "/blog"
+                        className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "nav-link"}
+                    >
+                        BLOGS
+                    </NavLink>
+                    <NavLink
+                        to = "/material"
+                         className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "nav-link"  }
+                    >
+                        MATERIALS
+                    </NavLink>
+                    <NavLink
+                        to = "/teams"
+                        className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "nav-link" }
+                    >
+                        TEAM
+                    </NavLink>
+                    <NavLink
+                        to="contact"
+                        className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "nav-link" }
+                    >
+                        CONTACT US
+                    </NavLink>
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Navbar
